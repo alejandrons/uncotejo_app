99510b4934a90a15c0f03d0b81bbe68a0775bd4e
ChewCEC <camilo.echeverri.100@gmail.com>
2025-02-19 21:20:28 -0500

    Vista Crear equipo terminada

--- uncotejo_front/lib/features/user/presentation/create_team_view.dart
+++ uncotejo_front/lib/features/user/presentation/create_team_view.dart
@@ -18,101 +18,132 @@ class CreateTeamScreen extends StatelessWidget {
         title: const Text('Create Team'),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              // First Box: Nuevo Equipo
-              Card(
-                elevation: 4,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Nuevo Equipo',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // First Box: Nuevo Equipo
+            Card(
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Nuevo Equipo',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
-                      const SizedBox(height: 16),
-                      TextField(
-                        decoration: const InputDecoration(
-                          labelText: 'Nombre de equipo',
-                          border: OutlineInputBorder(),
-                        ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Nombre de equipo',
+                        border: OutlineInputBorder(),
                       ),
-                      const SizedBox(height: 16),
-                      TextField(
-                        decoration: const InputDecoration(
-                          labelText: 'Descripcion',
-                          border: OutlineInputBorder(),
-                        ),
+                      onChanged: (value) {
+                        // Handle Nombre de equipo input
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Descripcion',
+                        border: OutlineInputBorder(),
                       ),
-                      const SizedBox(height: 16),
-                      TextField(
-                        decoration: const InputDecoration(
-                          labelText: 'Slogan',
-                          border: OutlineInputBorder(),
-                        ),
+                      onChanged: (value) {
+                        // Handle Descripcion input
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Slogan',
+                        border: OutlineInputBorder(),
                       ),
-                    ],
-                  ),
+                      onChanged: (value) {
+                        // Handle Slogan input
+                      },
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 20), // Spacing between boxes
-              // Second Box: Crear Escudo
-              Card(
-                elevation: 4,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Crear Escudo',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
+            ),
+            const SizedBox(height: 20), // Spacing between boxes
+            // Second Box: Crear Escudo
+            Card(
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Crear Escudo',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
-                      const SizedBox(height: 16),
-                      CarouselSlider(
-                        options: CarouselOptions(
-                          height: 150,
-                          autoPlay: true,
-                          enlargeCenterPage: true,
-                          viewportFraction: 0.5,
-                        ),
-                        items: shieldList.map((shield) {
-                          return Builder(
-                            builder: (BuildContext context) {
-                              return Container(
-                                width: MediaQuery.of(context).size.width,
-                                margin:
-                                    const EdgeInsets.symmetric(horizontal: 5.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  image: DecorationImage(
-                                    image: AssetImage(
-                                        shield),
-                                    fit: BoxFit.cover,
-                                  ),
+                    ),
+                    const SizedBox(height: 16),
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        height: 150,
+                        autoPlay: true,
+                        enlargeCenterPage: true,
+                        viewportFraction: 0.5,
+                      ),
+                      items: shieldList.map((shield) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                image: DecorationImage(
+                                  image: AssetImage(shield),
+                                  fit: BoxFit.cover,
                                 ),
-                              );
-                            },
-                          );
-                        }).toList(),
-                      ),
-                    ],
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 20), // Spacing between boxes and button
+            // Finalize Button
+            SizedBox(
+              width: double.infinity, // Make the button full width
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle the finalize action
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  backgroundColor: Colors.blue, // Customize button color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                child: const Text(
+                  'Finalizar',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: BottomAppBar(
@@ -148,10 +179,4 @@ class CreateTeamScreen extends StatelessWidget {
       ),
     );
   }
-}
-
-void main() {
-  runApp(const MaterialApp(
-    home: CreateTeamScreen(),
-  ));
 }
