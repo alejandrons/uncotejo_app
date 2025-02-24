db2b878446a60c42134821ac10f8f862592247ae
alejandrons <anoriega@unal.edu.co>
2025-02-23 20:50:58 -0500

    Setting up login files

--- uncotejo_front/lib/main.dart
+++ uncotejo_front/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uncotejo_front/features/auth/auth_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'UNCotejo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +29,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
+        primarySwatch: Colors.lightGreen,
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const AuthScreen(),
     );
   }
 }
